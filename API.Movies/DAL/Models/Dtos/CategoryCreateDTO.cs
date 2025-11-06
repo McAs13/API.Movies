2d08653dd5314896c6cacdd2f01cc208f073b485
Miguel Angel Santa <mcas@live.com.co>
2025-11-06 08:55:20 -0500

    Add DTOs, AutoMapper, and EF Core dependencies
    Introduced `CategoryCreateDTO` and `CategoryDTO` to standardize
    data transfer for the `Category` entity, with validation
    attributes to ensure data integrity. Added a `Mappers` class
    to configure object-to-object mappings using `AutoMapper`.
    
    Updated `API.Movies.csproj` to include `AutoMapper` and
    `EntityFrameworkCore` dependencies. Removed the unused
    `MoviesMapper` folder reference.

---
+++ API.Movies/DAL/Models/Dtos/CategoryCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Movies.DAL.Models.Dtos
+{
+    public class CategoryCreateDTO
+    {
+        [Required(ErrorMessage = "El nombre de la categoría es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El nombre de la categoría no puede exceder los 100 caracteres.")]
+        public string Name { get; set; }
+    }
+}
---
+++ API.Movies/DAL/Models/Dtos/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Movies.DAL.Models.Dtos
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre de la categoría es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El nombre de la categoría no puede exceder los 100 caracteres.")]
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
